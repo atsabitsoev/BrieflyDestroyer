6f78d29a98f8726c7acb97657188b3f4f56f2830
417 <417@sirenabus.com>
2020-01-24 13:55:56 +0300

    Initial Commit

---
+++ BrieflyDestroyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrieflyDestroyer
+//
+//  Created by Ацамаз Бицоев on 24.01.2020.
+//  Copyright © 2020 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
